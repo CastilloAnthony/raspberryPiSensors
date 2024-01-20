7b2686f134f1eea43fd43551a4e92df4824fc0fc
Anthony Castillo <anthony1093ca@gmail.com>
2024-01-20 00:29:43 -0800

    Testing...

--- dht_simpletest.py
+++ dht_simpletest.py
@@ -6,17 +6,14 @@ import board
 import adafruit_dht
 
 # Initial the dht device, with data pin connected to:
-# dhtDevice = adafruit_dht.DHT11(board.D32)
+dhtDevice = adafruit_dht.DHT11(board.D12)
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-# while True:
-for i in dir(board):
-    print('Trying '+str(i)+str(type(i)), type(board.D18))
-    dhtDevice = adafruit_dht.DHT11(board.i)
+while True:
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
